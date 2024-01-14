382755cafc219c7f7937f0d93664692b6f67d5de
Chawan Latif <147704132+Chawan1214@users.noreply.github.com>
2024-01-14 22:28:24 +0300

    update: replacing some images -changing few styles from style.css & login.css -editing signup and login file

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,3 +172,7 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+
+
